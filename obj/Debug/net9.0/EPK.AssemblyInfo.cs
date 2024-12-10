641f07cc18e5bfa198710f81513d2e57b37da767
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2024-12-10 17:08:46 -0600

    styling of navBar

--- obj/Debug/net9.0/EPK.AssemblyInfo.cs
+++ obj/Debug/net9.0/EPK.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EPK")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fdced545c47caeb40390f377e2612619cacdae5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+164817e20fa5711b65fdb9afd470b5be9e5221d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("EPK")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EPK")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
