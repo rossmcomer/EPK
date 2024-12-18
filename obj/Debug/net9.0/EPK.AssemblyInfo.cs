1da24fe8892349da5d3764405560ed2b2bfff9b6
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2024-12-17 22:14:15 -0600

    align navbar for different screen sizes

--- obj/Debug/net9.0/EPK.AssemblyInfo.cs
+++ obj/Debug/net9.0/EPK.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EPK")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f39545a00ae64b852ca10b1f44c9e572a471b2ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b31ca217f2d1eedd301e81405f7bf5f82f8cf5c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("EPK")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EPK")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
