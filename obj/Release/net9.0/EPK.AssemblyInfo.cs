73a2053ebcf7ed5747ddaca8a3c5d8c68d33f2f2
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2025-02-14 10:05:36 -0600

    update build to dist folder

--- obj/Release/net9.0/EPK.AssemblyInfo.cs
+++ obj/Release/net9.0/EPK.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EPK")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6fd51edb208fa707ac3db567c82f751ffa250dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88e522c36fa318b6c650a2da0a60a241355f2f46")]
 [assembly: System.Reflection.AssemblyProductAttribute("EPK")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EPK")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
