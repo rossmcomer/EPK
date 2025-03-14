9279ec6bf8b86ef76ebcc33df5e3f77d4844b349
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2025-03-13 19:56:07 -0500

    add name field to ContactFormModel

--- Models/ContactForm.cs
+++ Models/ContactForm.cs
@@ -3,6 +3,7 @@ namespace EPK.Models
     public class ContactFormModel
     {
         public string? RecipientEmail { get; set; }
+        public string? Name { get; set; }
         public string? Title { get; set; }
         public string? Message { get; set; }
     }
